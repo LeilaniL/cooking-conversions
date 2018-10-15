542a1a38c850ef417d6e5d05388ea3479233ad2f
Leilani Leach and Sandra Castaneda <sandra@>
2018-10-15 14:21:23 -0700

    add gallons to cups and other conversions

--- scripts.js
+++ scripts.js
@@ -8,4 +8,22 @@ var cups = parseInt (prompt ("How many cups do you have?"));
 var teaspoons = function (cups) {
   return cups * 48;
 }
-alert ("You have" + (teaspoons(cups)).toFixed (2) + " teaspoons.");
+alert ("You have " + (teaspoons(cups)).toFixed (2) + " teaspoons.");
+
+var teaspoons2 = parseInt(prompt("How many teaspoons do you have?"));
+var tablespoons = function (teaspons2) {
+  return teaspoons2/3
+}
+alert ("You have " + (tablespoons(teaspoons2)).toFixed (2) + " tablespoons.");
+
+var tablespoons = parseInt(prompt("How many tablespoons do you have?"));
+var teaspoons2 = function (tablespoons) {
+  return tablespoons*3
+}
+alert ("You have " + (teaspoons2(tablespoons)).toFixed (2) + " teaspoons.");
+
+var howManyGallons = parseInt(prompt("How many gallons do you have?"));
+var cupsFromGallons = function(howManyGallons){
+  return howManyGallons*16;
+}
+alert("That's " + (cupsFromGallons(howManyGallons)) + " cups!");
