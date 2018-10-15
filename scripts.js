4297fdab6268a50f6325dfac0416ae0b65e5b7ae
Leilani Leach and Sandra Castaneda <leilani@>
2018-10-15 15:47:38 -0700

    add jQuery to display alert when clicking on header

--- scripts.js
+++ scripts.js
@@ -27,3 +27,9 @@ var cupsFromGallons = function(howManyGallons){
   return howManyGallons*16;
 }
 alert("That's " + (cupsFromGallons(howManyGallons)) + " cups!");
+$(document) . ready (function() {
+  $("h1").click(function() {
+    alert("This is a header.");
+    alert("I told you, THIS IS A HEADER!");
+  });
+});
